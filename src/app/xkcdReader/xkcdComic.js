69d6f2072006c6206221c8e809bf9e7738ac4e2f
unknown <dst33nburgh@gmail.com>
2015-08-30 22:13:36 -0500

    Added title and alt(tooltip) text from JSON data. closes #6

--- src/app/xkcdReader/xkcdComic.js
+++ src/app/xkcdReader/xkcdComic.js
@@ -8,7 +8,8 @@
             scope: {
                 comicNumber: "="
             },
-            template:   "<img xkcd-show-loader='loading' class='centered-block' ng-src='{{imgHref}}' >",
+            template:   "<h2 class='center-text'>{{title}}</h2>" +
+                        "<img xkcd-show-loader='loading' class='centered-block' title='{{alt}}' ng-src='{{imgHref}}' >",
             link: function (scope, elem) {
                 elem.children().on("load", function() {
                     $rootScope.$apply(function() {scope.loading = false;});
@@ -20,6 +21,8 @@
                         scope.loading = true;
                         xkcdComicService.getComicAsync(newVal).then(function(comicData) {
                             scope.imgHref = comicData.img;
+                            scope.title = comicData.safe_title;
+                            scope.alt = comicData.alt;
                         });
                     }
                 });
